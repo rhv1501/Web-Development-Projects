332c92f1f9d329b1087f1bc873d9e5159455d9b2
rhv1501 <129303708+rhv1501@users.noreply.github.com>
2023-08-29 09:41:58 +0530

    Add files via upload

---
+++ calculator/main.js
@@ -0,0 +1,31 @@
+let input =document.getElementById('text');
+let buttons =document.querySelectorAll
+('button');
+let str="";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click',(e) => {
+        if(e.target.innerHTML == '%')
+        {
+            var str1 = str.indexOf("%")
+            str1= str / 100
+            input.value = str + str1;
+        }
+        else if(e.target.innerHTML == '='){
+            str = eval(str);
+            input.value = str;
+        }
+        else if(e.target.innerHTML == 'AC'){
+            str = "";
+            input.value = str;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            str = str.substring(0,str.length-1);
+            input.value = str;
+        }
+        else{
+            str=str+e.target.innerHTML;
+            input.value = str;
+        }
+    })
+})
